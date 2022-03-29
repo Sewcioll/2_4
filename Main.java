da112fabc1653300f73ed7ee54d8a82a149c2ad2
Sewcioll <100691797+Sewcioll@users.noreply.github.com>
2022-03-29 11:26:47 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args) {
+    int x=10,y=10;
+    for(int i=1;i<=x;i++)
+      {
+        for(int j=1;j<=y;j++)
+          {
+            int wynik =i*j;
+             System.out.print("  "+wynik);
+          }
+         System.out.println();
+      }
+     
+    
+   
+  }
+}
